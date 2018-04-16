923286007e174d865cc0e20c7f6f40aa1c49d63b
irodyk <iurii.rodyk@gmail.com>
2018-04-16 12:47:48 +0200

    remove parcelize annotation to fix travis builds

--- presentation/src/main/kotlin/com/yurets/chucknorrisfunapp/viewmodel/JokeViewModel.kt
+++ presentation/src/main/kotlin/com/yurets/chucknorrisfunapp/viewmodel/JokeViewModel.kt
@@ -1,13 +1,39 @@
 package com.yurets.chucknorrisfunapp.viewmodel
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class JokeViewModel(val id: Int, val text: String, val userVote: Int, val overallRating: Int)
     : Parcelable, Comparable<JokeViewModel>{
 
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readInt())
+
     override fun compareTo(other: JokeViewModel): Int {
         return id.compareTo(other.id)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(text)
+        parcel.writeInt(userVote)
+        parcel.writeInt(overallRating)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<JokeViewModel> {
+        override fun createFromParcel(parcel: Parcel): JokeViewModel {
+            return JokeViewModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<JokeViewModel?> {
+            return arrayOfNulls(size)
+        }
     }
 }
